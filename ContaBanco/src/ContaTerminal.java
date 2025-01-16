c1942ded126e0ce7f5485cfb2abe52f0e4c5ff7b
CawanDEV <cawandev@gmail.com>
2025-01-16 14:31:14 -0300

    feat: desafio projeto conta bancaria

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        double saldo = 1865.21;
+
+        System.out.println("Digite seu Número de Usuário");
+        int usuario = scanner.nextInt();
+
+        System.out.println("Digite sua Agência");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Digite seu Nome");
+        String nome = scanner.next();
+
+        System.out.println("Boa tarde Sr(a) " + nome + " Obrigado por criar uma conta em nosso banco, sua Agência é " + agencia + " conta " + usuario + " e seu saldo de " + saldo +" já está disponivel para o saque!");
+
+    }
+}
